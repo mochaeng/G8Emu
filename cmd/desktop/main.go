1e3687ff8af0f0e6531c8caa8b5b828ccf94dd61
Luiz Campos <campos.antoonio@gmail.com>
2025-08-08 15:32:25 -0300

    fix: removing unecessary canvas

--- cmd/desktop/main.go
+++ cmd/desktop/main.go
@@ -1,43 +1,48 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/mochaeng/G8Emu/internal/constants"
+	"github.com/mochaeng/G8Emu/internal/core"
+	"github.com/mochaeng/G8Emu/internal/emulator"
+)
+
 func main() {
-	// if len(os.Args) != 3 {
-	// 	fmt.Fprintf(os.Stderr, "Usage: %s <Scale> <Delay> <ROM>\n", os.Args[0])
-	// 	fmt.Fprintf(os.Stderr, "   Scale: Integer scale factor (e.g., 10)\n")
-	// 	// fmt.Fprintf(os.Stderr, "   Delay: Milliseconds between cycles (e.g., 1)\n")
-	// 	fmt.Fprintf(os.Stderr, "   ROM: Path to ROM file\n")
-	// 	os.Exit(1)
-	// }
+	if len(os.Args) != 3 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <Scale> <Delay> <ROM>\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "   Scale: Integer scale factor (e.g., 10)\n")
+		// fmt.Fprintf(os.Stderr, "   Delay: Milliseconds between cycles (e.g., 1)\n")
+		fmt.Fprintf(os.Stderr, "   ROM: Path to ROM file\n")
+		os.Exit(1)
+	}
 
-	// videoScale, err := strconv.Atoi(os.Args[1])
-	// if err != nil {
-	// 	log.Fatalf("invalid scale factor: %v", err)
-	// }
+	videoScale, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatalf("invalid scale factor: %v", err)
+	}
 
-	// cycleDelay, err := strconv.Atoi(os.Args[2])
-	// if err != nil {
-	// 	log.Fatalf("invalid cycle delay: %v", err)
-	// }
+	romFilename := os.Args[2]
 
-	// romFilename := os.Args[2]
+	platform := emulator.NewPlatform(videoScale)
+	chip8 := core.NewChip8()
 
-	// videoScale := 15
-	// romFilename := "ROMs/test_opcode.ch8"
-	// platform := system.NewPlatform(videoScale)
-	// chip8 := core.NewChip8()
+	if err := chip8.LoadRomFile(romFilename); err != nil {
+		log.Fatalf("failed to load ROM: %v", err)
+	}
 
-	// if err := chip8.LoadRomFile(romFilename); err != nil {
-	// 	log.Fatalf("failed to load ROM: %v", err)
-	// }
+	ebiten.SetWindowSize(constants.VIDEO_WIDTH*videoScale, constants.VIDEO_HEIGHT*videoScale)
+	ebiten.SetWindowTitle("G8Emu")
 
-	// ebiten.SetWindowSize(constants.VIDEO_WIDTH*videoScale, constants.VIDEO_HEIGHT*videoScale)
-	// ebiten.SetWindowTitle("G8Emu")
+	cpuFrequency := 540
+	game := emulator.NewGame(platform, chip8, cpuFrequency)
 
-	// cpuFrequency := 540
-	// game := emulator.NewGame(platform, chip8, cpuFrequency)
-
-	// if err := ebiten.RunGame(game); err != nil {
-	// 	log.Fatal(err)
-	// }
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
 
 }
